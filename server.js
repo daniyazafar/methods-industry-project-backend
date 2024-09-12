d99aada7928775647613792be810982e699678ad
daniyazafar <daniyazafar@gmail.com>
2024-09-12 03:07:28 -0700

    initializing backend

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const { PORT } = process.env || 5050;
+
+app.get('/', (_req, res) => {
+    res.send("Welcom to the backend!")
+});
+
+app.listen(PORT, () => {
+    console.log("Listening to port: ", PORT)
+})
