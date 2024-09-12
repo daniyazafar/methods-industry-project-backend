ea62ec3fe87243b8724bbbedb7fb1e265fe44cc5
daniyazafar <daniyazafar@gmail.com>
2024-09-12 03:20:35 -0700

    added knex to setup

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,11 @@ import knex from 'knex';
 import 'dotenv/config'
 
 const db = knex({
-  client: 'mysql2', // Specify mysql2 as the client
+  client: 'mysql2',
   connection: {
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'your_database_user',
-    password: process.env.DB_PASSWORD || 'your_database_password',
-    database: process.env.DB_NAME || 'your_database_name'
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'rootroot',
+    database: process.env.DB_NAME || 'industry_proj_db'
   }
 });
